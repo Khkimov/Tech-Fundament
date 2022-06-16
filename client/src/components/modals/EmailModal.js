7d730ec6f525eec0c2d8942da9e2a9c268d8617d
Ilyas <khakim198765@gmail.com>
2022-06-16 21:42:58 +0300

    Create send email function

--- client/src/components/modals/EmailModal.js
+++ client/src/components/modals/EmailModal.js
@@ -1,11 +1,26 @@
-import React, {useState} from "react";
-import {Modal, Button, Form} from 'react-bootstrap';
+import React, {useState, useRef} from "react";
+import {Modal, Button} from 'react-bootstrap';
+import emailjs from '@emailjs/browser';
 
 function EmailModal() {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_fabaf7g', 'template_brebtxm', form.current, 'bQLwenvccnFw8Fqbm')
+      .then((result) => {
+          console.log(result.text);
+          e.target.reset();
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
 
   return (
     <>
@@ -18,16 +33,15 @@ function EmailModal() {
           <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <Form>
-  <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="name@example.com" />
-  </Form.Group>
-  <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-    <Form.Label>Example textarea</Form.Label>
-    <Form.Control as="textarea" rows={3} />
-  </Form.Group>
-</Form>
+        <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
